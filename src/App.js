f957d06ef0251f3aec18cd125fa58ed8b8106a48
fernando peguero <fernandoapeguero@gmail.com>
2021-01-28 19:52:03 -0500

    Set up the structure of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route path='/signin' >
+             <p> I WIll be the sign in page</p>
+          </Route>
+          <Route path='/signup' >
+             <p> I WIll be the sign up page</p>
+          </Route>
+          <Route path='/browse' >
+             <p> I WIll be the browse page</p>
+          </Route>
+          <Route path='/' >
+             <p>I am going to be a cloned Netflix Application</p>
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
