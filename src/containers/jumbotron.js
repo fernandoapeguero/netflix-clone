4f00e2d1a62d5fa9de6cb62d06ec99573409148d
fernando peguero <fernandoapeguero@gmail.com>
2021-01-31 18:17:46 -0500

    working home page displaying images

---
+++ src/containers/jumbotron.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Jumbotron from '../components/jumbotron';
+import { Item } from '../components/jumbotron/Styles/jumbotron';
+import jumboData from '../fixtures/jumbo.json'
+
+export function JumbotronContainer() {
+    return (
+        <Jumbotron.Container>
+        {jumboData.map((item) => (
+            <Jumbotron key={item.id} direction={Item.direction}>
+                <Jumbotron.Pane>
+                    <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                    <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
+                </Jumbotron.Pane>
+                <Jumbotron.Pane>
+                    <Jumbotron.Image src={item.image} alt={item.alt} />
+                </Jumbotron.Pane>
+               
+            </Jumbotron>
+        ))} 
+        </Jumbotron.Container>
+    )
+}
