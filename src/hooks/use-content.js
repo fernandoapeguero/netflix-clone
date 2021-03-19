635de5355dc6bc2ea0eb1d65c8e5d244d65115d1
fernando peguero <fernandoapeguero@gmail.com>
2021-03-19 16:34:43 -0400

    fix issues with firebase not loading browse page and adde utils

--- src/hooks/use-content.js
+++ src/hooks/use-content.js
@@ -1,5 +1,6 @@
 import { useEffect, useState, useContext } from 'react';
 import { FirebaseContext } from '../context/firebase';
+import 'firebase/firestore';
 
 export default function useContent(target) {
     const [content, setContent] = useState([]);
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {BrowseContainer} from '../containers/browse';
 import {useContent} from '../hooks';
-
+import {selectionMap} from '../utils'
 
 export default function Browse(){
     const {series} = useContent('series');
     const {films} = useContent('films');
 
-    console.log(series);
-    console.log(films);
+    const slides = selectionMap({series, films})
+
+    console.log(slides)
     
     return (
-        <BrowseContainer />
+        <BrowseContainer slides={slides} />
     )
 }
